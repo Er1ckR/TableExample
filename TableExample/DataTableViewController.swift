7335fb305a0e8ba16ca048ec655f7c53d70491da
erick.rodriguez.pm@gmail.com <er1ckr519@gmail.com>
2015-10-22 16:31:49 -0400

    Updated TableExample files

--- TableExample/DataTableViewController.swift
+++ TableExample/DataTableViewController.swift
@@ -9,33 +9,29 @@
 import UIKit
 
 class DataTableViewController: UITableViewController {
-
-
-    // MARK: - Table view data source
-
+    
+    // MARK: - UITableViewDataSource
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        // Return the number of sections.
         return 3
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        // Return the number of rows in the section.
         return 5
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Section \(section)"
     }
-
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("LabelCell", forIndexPath: indexPath)
-
-        // Configure the cell...
+        
         cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
         
         return cell
     }
     
-
 }
